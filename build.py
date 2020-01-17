9d41356e572bbc5b8eccb25468893a360244940a
Dan Weatherill <plasteredparrot@gmail.com>
2020-01-17 23:16:34 +0000

    fix argument in build.py

--- build.py
+++ build.py
@@ -4,6 +4,6 @@ from bincrafters import build_template_default
 
 if __name__ == "__main__":
 
-    builder = build_template_default.get_builder(c_only=True)
+    builder = build_template_default.get_builder(pure_c=True)
 
     builder.run()

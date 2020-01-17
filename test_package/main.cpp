b268b286aafbb538d0afac99e5282c79912353d8
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2020-01-17 16:46:38 +0000

    some work on test package

--- test_package/main.cpp
+++ test_package/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <fitsio.h>
+
 
 using std::cout;
 using std::endl;
 
 int main(int argc, char** argv)
 {
-  
-  cout << "hello world" << endl;
+  float vers;
+  float stat  = fits_get_version(&vers);
+  cout << "fitsio version is:" << vers <<  endl;
 }
